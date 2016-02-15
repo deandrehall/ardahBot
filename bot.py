1a8f6e67d486e07ef88fed43089188002e664724
deandrehall <deandrehall00@gmail.com>
2016-02-15 15:30:30 -0600

    everything is much neater now

--- bot.py
+++ bot.py
@@ -234,40 +234,30 @@ def commands():
     if message == '!meme_me':
         generatememe()
 
+
 sendmessage('it that bot')
 
+
 while True:
-    readbuffer = readbuffer + s.recv(1024)
-    temp = string.split(readbuffer, "\n")
-    readbuffer = temp.pop()
+    try:
+        readbuffer = readbuffer+s.recv(1024)
+        temp = string.split(readbuffer, "\n")
+        readbuffer = temp.pop()
 
-    for line in temp:
-        # Checks whether the message is PING because its a method of Twitch to check if you're afk
-        if (line[0] == "PING"):
-            s.send("PONG %s\r\n" % line[1])
-        else:
-            # Splits the given string so we can work with it better
-            parts = string.split(line, ":")
-
+        for line in temp:
+            if line[0] == "PING":
+                    s.send("PONG %s\r\n" % line[1])
+            else:
+                parts = string.split(line, ":")  # Splits the given string so we can work with it better
             if "QUIT" not in parts[1] and "JOIN" not in parts[1] and "PART" not in parts[1]:
                 try:
-                    # Sets the message variable to the actual message sent
-                    message = parts[2][:len(parts[2]) - 1]
+                    message = parts[2][:len(parts[2]) - 1]  # Sets the message variable to the actual message sent
                 except:
                     message = ""
-                # Sets the username variable to the actual username
-                usernamesplit = string.split(parts[1], "!")
-                username = usernamesplit[0]
+                    usernamesplit = string.split(parts[1], "!")
+                    username = usernamesplit[0]  # Sets the username variable to the actual username
 
-                # Only works after twitch is done announcing stuff (MODT = Message of the day)
-                if MODT:
-                    print username + ": " + message
+        commands()  # checks if a command has been called
 
-                    ########################### Commands #############################
-
-                    commands()
-
-                    #################################################################
-                for l in parts:
-                    if "End of /NAMES list" in l:
-                        MODT = True
+    except:
+        print traceback.format_exc()
